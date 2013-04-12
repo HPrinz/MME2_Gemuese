cb749c86588f65a8612499e3b28810a8239ecf35
Hanna <hanna_prinz@yahoo.de>
2013-04-12 13:35:18 +0200

    Webseitentitel hinzugefuegt, Map auch ohne Positionierung dargestellt (Zentiert am Alex)

--- client/js/main.js
+++ client/js/main.js
@@ -1,17 +1,17 @@
 function $(id) {
     return document.getElementById(id);
 }
- 
+
 window.onload = function() {
     $('link').onclick = function() {
-       getHttpRequest();
-       return false;
-    }
+        getHttpRequest();
+        return false;
+    };
 };
 
 function getHttpRequest() {
     var xmlhttp = null;
-    
+
     // Mozilla
     if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
@@ -20,19 +20,19 @@ function getHttpRequest() {
     else if (window.ActiveXObject) {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-   
+
     xmlhttp.open("GET", 'http://127.0.0.1:9090/gemuese/gemueseREST/list', true);
     xmlhttp.onreadystatechange = function() {
-        if(xmlhttp.readyState != 4) {
+        if (xmlhttp.readyState != 4) {
             $('ergebnis').innerHTML = 'Seite wird geladen ...';
         }
-        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var response = JSON.parse(xmlhttp.responseText);
             var string = "";
 
-            for(var i = 0; i < response.fruits.length; i++) {
+            for ( var i = 0; i < response.fruits.length; i++) {
                 var temp = JSON.parse(response.fruits[i]);
-                string = string + " " + temp.name
+                string = string + " " + temp.name;
             }
             $('ergebnis').innerHTML = string;
         }
