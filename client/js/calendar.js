a13e7ce37b978f3f9f125531d6f3874aa9c0efa2
Hanna <hanna_prinz@yahoo.de>
2013-04-30 22:26:10 +0200

    Kalender fragt die Datenbank ab und zeigt die Saison richtig an.

--- client/js/calendar.js
+++ client/js/calendar.js
@@ -1,265 +1,213 @@
+function makeHttpRequest() {
+	var xmlhttp = new XMLHttpRequest();
+	var f = [];
 
-var fruitsArray = makeHttpRequest();
+	xmlhttp.open("GET", 'http://127.0.0.1:9090/gemuese/gemueseREST/list', true);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState != 4) {
 
-// four colors
-var buckets = 4,
-	colorScheme = 'rbow2',
-	days = [
-		{ name: 'Apfel', abbr: 'Apfel' },
-		{ name: 'Erdbeere', abbr: 'Erdbeere' },
-		{ name: 'Kartoffel', abbr: 'Kartoffel' },
-		{ name: 'Pilze', abbr: 'Pilze' },
-		{ name: 'Salat', abbr: 'Salat' },
-		{ name: 'Rote Bete', abbr: 'Rote Bete' },
-		{ name: 'Gurke', abbr: 'Gurke' }
-	],
-	types = {
-		all: 'Alle',
-		pc: 'Gemüse',
-		mob: 'Obst'
-	},
-	hours = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'];
-	
-var data;
-
-d3.select('#vis').classed(colorScheme, true);
-
-d3.json('tru247.json', function(json) {
-	
-	data = json;
-
-	createTiles();
-	reColorTiles('all', 'all');
-
-	/* ************************** */
-	
-	// All, PC, Mobile control event listener
-	$('input[name="type"]').change(function() {
-		
-		var type = $(this).val(),
-			$sel = d3.select('#map path.state.sel');
-		
-		d3.selectAll('fieldset#type label').classed('sel', false);
-		d3.select('label[for="type_' + type + '"]').classed('sel', true);
-		
-		if ($sel.empty()) {
-			var state = 'all';
-		} else {
-			var state = $sel.attr('id');
 		}
-		
-		reColorTiles(state, type);
-		d3.select('#pc2mob').attr('class', type);
-		
-		var type = types[selectedType()];
-		d3.select('#wtf .subtitle').html(type  + ' traffic daily');
-	});
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var response = JSON.parse(xmlhttp.responseText);
+
+			for ( var i = 0; i < response.fruits.length; i++) {
+
+				var temp = JSON.parse(response.fruits[i]);
+				f.push(temp);
+			}
+			initVariables(f);
+		}
+	};
+	xmlhttp.send("");
+
+}
+
+makeHttpRequest();
+
+function initVariables(f) {
+
+	var fruitsArray = f;
+
+	console.log("fruitsArray = " + fruitsArray);
+
+	// four colors
+	var buckets = 4;
+
+	var colorScheme = 'rbow2';
+
+	var fruits = [ {
+		abbr : 'Apfel'
+	}, {
+		abbr : 'Erdbeere'
+	}, {
+		abbr : 'Kartoffel'
+	}, {
+		abbr : 'Pilze'
+	}, {
+		abbr : 'Salat'
+	}, {
+		abbr : 'Rote Bete'
+	}, {
+		abbr : 'Gurke'
+	} ];
+
+	var types = {
+		obst : 'Alle',
+		gemuese : 'Gemüse',
+		mob : 'Obst'
+	};
+
+	months = [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep',
+			'Okt', 'Nov', 'Dez' ];
 	
+	monthsEng = [ 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep',
+				'oct', 'nov', 'dec' ];
+
+	// for the colors TODO what is vis?
+	d3.select('#vis').classed(colorScheme, true);
+
+	createTiles("all", fruitsArray);
+	reColorTiles('obst', fruitsArray, buckets);
 
 	/* ************************** */
-	
+
+	// All, Obst, Gemüse button event listener
+	$('input[name="type"]').change(function() {
+
+		var type = $(this).val();
+
+		console.log("type = " + type);
+
+		state = "obst";
+
+		createTiles(type, fruitsArray);
+		reColorTiles(state, fruitsArray, buckets);
+	});
+
+	/* ************************** */
+
 	// tiles mouseover events
 	$('#tiles td').hover(function() {
-	
 		$(this).addClass('sel');
-		
-		var tmp = $(this).attr('id').split('d').join('').split('h'),
-			day = parseInt(tmp[0]),
-			hour = parseInt(tmp[1]);
-		
-		var $sel = d3.select('#map path.state.sel');
-		
-		var state = 'all';
-		var view = 'all';
 
-		var type = types[selectedType()];
-		d3.select('#wtf .subtitle').html(type  + ' traffic on ' + days[day].name + 's');
-	
 	}, function() {
-		
 		$(this).removeClass('sel');
-		
-		var $sel = d3.select('#map path.state.sel');
-		
-		var state = 'all';
-
-		var type = types[selectedType()];
-		d3.select('#wtf .subtitle').html(type  + ' traffic daily');
 	});
-});
-
-/* ************************** */
-
-function makeHttpRequest() {
-    var xmlhttp = new XMLHttpRequest();
-    this.fruits = new Array();
-
-    xmlhttp.open("GET", 'http://127.0.0.1:9090/gemuese/gemueseREST/list', true);
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState != 4) {
-            
-        }
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var response = JSON.parse(xmlhttp.responseText);
-            var string = "";
-
-            for ( var i = 0; i < response.fruits.length; i++) {
-                
-                var temp = JSON.parse(response.fruits[i]);
-                fruits.push(temp);
-            }
-        }
-    };
-    xmlhttp.send(null);
-    
-    return fruits;
 }
 
 /* ************************** */
 
 function selectedType() {
-	
-	//return d3.select('input[name="type"]:checked').property('value'); // IE8 doesn't like this
+
+	// return d3.select('input[name="type"]:checked').property('value'); // IE8
+	// doesn't like this
 	return $('input[name="type"]:checked').val();
 }
 
 /* ************************** */
 
-function getCalcs(state, view) {
-	
-	var min = 1,
-		max = -1;
-	
-	// calculate min + max
-	for (var d = 0; d < data[state].views.length; d++) {
-		for (var h = 0; h < data[state].views[d].length; h++) {
-			
-			if (view === 'all') {
-				var tot = data[state].views[d][h].pc + data[state].views[d][h].mob;
-			} else {
-				var tot = data[state].views[d][h][view];
-			}
-			
-			if (tot > max) {
-				max = tot;
-			}
-			
-			if (tot < min) {
-				min = tot;
-			}
-		}
-	}
-	
-	return {'min': min, 'max': max};
-};
+function getCalcs() {
 
-function getSeason(vegetable) {
-	
-	var min = 1,
-		max = -1;
-	
-	// calculate min + max
-	for (var d = 0; d < data[state].views.length; d++) {
-		for (var h = 0; h < data[state].views[d].length; h++) {
-			
-			if (view === 'all') {
-				var tot = data[state].views[d][h].pc + data[state].views[d][h].mob;
-			} else {
-				var tot = data[state].views[d][h][view];
-			}
-			
-			if (tot > max) {
-				max = tot;
-			}
-			
-			if (tot < min) {
-				min = tot;
-			}
-		}
-	}
-	
-	return {'min': min, 'max': max};
+	var min = 0, max = 3;
+
+	return {
+		'min' : min,
+		'max' : max
+	};
 };
 
 /* ************************** */
 
-function reColorTiles(state, view) {
-	
-	var calcs = getCalcs(state, view),
-		range = [];
-	
-	for (var i = 1; i <= buckets; i++) {
+function reColorTiles(state, fruitsArray, buckets) {
+
+	var calcs = getCalcs(), range = [];
+
+	for ( var i = 1; i <= buckets; i++) {
 		range.push(i);
 	}
-	
-    console.log("range: " + range);
-    console.log("calcs: " +calcs.min + "/" + calcs.max);
-    
-	var bucket = d3.scale.quantize().domain([0, 0.02]).range(range),
-		side = d3.select('#tiles').attr('class');
-	
-	
+
+//	console.log("range: " + range);
+//	console.log("calcs: " + calcs.min + "/" + calcs.max);
+
+	var bucket = d3.scale.quantize().domain([ 0, 4 ]).range(range);
+	var side = d3.select('#tiles').attr('class');
+
 	if (side === 'front') {
 		side = 'back';
 	} else {
 		side = 'front';
 	}
-	
-	for (var d = 0; d < data[state].views.length; d++) {
-		for (var h = 0; h < data[state].views[d].length; h++) {
 
-			var sel = '#d' + d + 'h' + h + ' .tile .' + side,
-				val = data[state].views[d][h].pc + data[state].views[d][h].mob;
+	console.log("length: " + fruitsArray[0].season.length);
+	
+	for ( var d = 0; d < fruitsArray.length; d++) {
+		for ( var h = 0; h <= monthsEng.length; h++) {
 			
-			if (view !== 'all') {
-				val = data[state].views[d][h][view];
+			var sel = '#d' + d + 'h' + h + ' .tile .' + side;
+
+			var mon = monthsEng[h]
+			
+			var val = fruitsArray[d].season[mon];
+			
+			if (val === "none") {
+				val = 3;
+			} else if (val === "storage") {
+				val = 2;
+			} else if (val === "greenhouse") {
+				val = 1;
+			} else if (val === "fresh"){
+				val = 0;
 			}
+
+//			console.log("sel: " + sel);
+//			console.log("val: " + val);
 			
 			// erase all previous bucket designations on this cell
-			for (var i = 1; i <= buckets; i++) {
+			for ( var i = 1; i <= buckets; i++) {
 				var cls = 'q' + i + '-' + buckets;
-				d3.select(sel).classed(cls , false);
+				d3.select(sel).classed(cls, false);
 			}
-			
+
 			// set new bucket designation for this cell
-			var cls = 'q' + (val > 0 ? bucket(val) : 0) + '-' + buckets;
-            
+			var cls = 'q' + (val > 0 ? bucket(val) : 1) + '-' + buckets;
+			
+//			console.log("cls = " + (val > 0 ?bucket(val) : 0) + "-" + buckets);
+
 			d3.select(sel).classed(cls, true);
 		}
 	}
-	flipTiles();
+	flipTiles(fruitsArray);
 }
 
 /* ************************** */
 
-function flipTiles() {
+function flipTiles(fruitsArray) {
 
-	var oldSide = d3.select('#tiles').attr('class'),
-		newSide = '';
-	
+	var oldSide = d3.select('#tiles').attr('class'), newSide = '';
+
 	if (oldSide == 'front') {
 		newSide = 'back';
 	} else {
 		newSide = 'front';
 	}
-	
+
 	var flipper = function(h, d, side) {
 		return function() {
-			var sel = '#d' + d + 'h' + h + ' .tile',
-				rotateY = 'rotateY(180deg)';
-			
+			var sel = '#d' + d + 'h' + h + ' .tile', rotateY = 'rotateY(180deg)';
+
 			if (side === 'back') {
-				rotateY = 'rotateY(0deg)';	
+				rotateY = 'rotateY(0deg)';
 			}
 
-				d3.select(sel).style('-webkit-transform', rotateY);
+			d3.select(sel).style('-webkit-transform', rotateY);
 		};
 	};
-	
-	for (var h = 0; h < hours.length; h++) {
-		for (var d = 0; d < days.length; d++) {
+
+	for ( var h = 0; h < months.length; h++) {
+		for ( var d = 0; d < fruitsArray.length; d++) {
 			var side = d3.select('#tiles').attr('class');
-			setTimeout(flipper(h, d, side), (h * 20) + (d * 20) + (Math.random() * 100));
+			setTimeout(flipper(h, d, side), (h * 20) + (d * 20)
+					+ (Math.random() * 100));
 		}
 	}
 	d3.select('#tiles').attr('class', newSide);
@@ -267,48 +215,40 @@ function flipTiles() {
 
 /* ************************** */
 
-function updateIE8percents(state) {
-
-	var rawMobPercent = 100 / (data[state].pc2mob + 1);
-	
-	if (rawMobPercent < 1) {
-		var mobPercent = '< 1',
-			pcPercent = '> 99';
-	} else {
-		var mobPercent = Math.round(rawMobPercent),
-			pcPercent = 100 - mobPercent;
-	}
-	
-	d3.select('#pc2mob .pc span').html(pcPercent + '%');
-	d3.select('#pc2mob .mob span').html(mobPercent + '%');
-	
-	var html = d3.select('#pc2mob ul').html();
-	d3.select('#ie8_percents').html(html);
-}
-
-/* ************************** */
-
-function createTiles() {
+function createTiles(type, fruitsArray) {
 
 	var html = '<table id="tiles" class="front">';
 
 	html += '<tr><th><div>&nbsp;</div></th>';
 
-	for (var h = 0; h < hours.length; h++) {
-		html += '<th class="h' + h + '">' + hours[h] + '</th>';
+	for ( var h = 0; h < months.length; h++) {
+		html += '<th class="h' + h + '">' + months[h] + '</th>';
 	}
-	
+
 	html += '</tr>';
 
-	for (var d = 0; d < days.length; d++) {
-		html += '<tr class="d' + d + '">';
-		html += '<th>' + days[d].abbr + '</th>';
-		for (var h = 0; h < hours.length; h++) {
-			html += '<td id="d' + d + 'h' + h + '" class="d' + d + ' h' + h + '"><div class="tile"><div class="face front"></div><div class="face back"></div></div></td>';
+	for ( var d = 0; d < fruitsArray.length; d++) {
+		if ((fruitsArray[d].vegetable == false && type == "obst")
+				|| (fruitsArray[d].vegetable == true && type == "gemuese")
+				|| type == "all") {
+			html += '<tr class="d' + d + '">';
+			html += '<th>' + fruitsArray[d].name + '</th>';
+			for ( var h = 0; h < months.length; h++) {
+				html += '<td id="d'
+						+ d
+						+ 'h'
+						+ h
+						+ '" class="d'
+						+ d
+						+ ' h'
+						+ h
+						+ '"><div class="tile"><div class="face front"></div><div class="face back"></div></div></td>';
+			}
+
+			html += '</tr>';
 		}
-		html += '</tr>';
 	}
-	
+
 	html += '</table>';
 	d3.select('#vis').html(html);
 }
